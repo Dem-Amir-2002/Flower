0880f2da794912806d2ba92215cd68677c2ea732
Amir Damirchi <154810287+Dem-Amir-2002@users.noreply.github.com>
2025-08-27 19:02:52 +0330

    somthing change !

--- index.php
+++ index.php
@@ -21,11 +21,11 @@ $cat = $conn->query("SELECT * FROM `categories` ORDER BY `cname` ASC");
                 <div class="d-flex text-justify">
                     <div class="vr text-success" style="border-left:3px solid"></div>
                     <p class="px-3">به دنیای سرسبز ما خوش آمدید! <br>
-در اینجا می‌توانید مجموعه‌ای زیبا از گیاهان و گل‌های تازه را پیدا کنید که هر فضایی را دلنشین می‌کنند. فرقی نمی‌کند به دنبال هدیه‌ای خاص هستید یا می‌خواهید گوشه‌ای از طبیعت را به خانه خود بیاورید، ما برای هر سلیقه‌ای گزینه‌ای داریم. <br>با ما، زندگی خود را سبزتر کنید.
+                        در اینجا می‌توانید مجموعه‌ای زیبا از گیاهان و گل‌های تازه را پیدا کنید که هر فضایی را دلنشین می‌کنند. فرقی نمی‌کند به دنبال هدیه‌ای خاص هستید یا می‌خواهید گوشه‌ای از طبیعت را به خانه خود بیاورید، ما برای هر سلیقه‌ای گزینه‌ای داریم. <br>با ما، زندگی خود را سبزتر کنید.
                     </p>
                 </div>
                 <div class="text-start m-3">
-                <a href="products.php" class="btn btn-success">مشاهده محصولات <i class="fa fa-tag"></i> </a>
+                    <a href="products.php" class="btn btn-success">مشاهده محصولات <i class="fa fa-tag"></i> </a>
                 </div>
             </div>
             <div class="col-md-6">
@@ -60,13 +60,13 @@ $cat = $conn->query("SELECT * FROM `categories` ORDER BY `cname` ASC");
             </div>
         </div>
         <div class="row mt-5 mb-5 p-4 bg-body rounded shadow-sm text-center">
-            <?php while($crows = $cat->fetch()) { ?>
-            <div class="col-md-2 col-6">
-                <a href="products.php?cat=<?=$crows['cid']?>">
-                <img class="shadow-sm p-2 img-thumbnail btn btn-outline-light" src="img/cat/<?=$crows['pic']?>">
-                <h6 class="mt-2 text-muted"><?=$crows['cname']?></h6>
-                </a>
-            </div>
+            <?php while ($crows = $cat->fetch()) { ?>
+                <div class="col-md-2 col-6">
+                    <a href="products.php?cat=<?= $crows['cid'] ?>">
+                        <img class="shadow-sm p-2 img-thumbnail btn btn-outline-light" src="img/cat/<?= $crows['pic'] ?>">
+                        <h6 class="mt-2 text-muted"><?= $crows['cname'] ?></h6>
+                    </a>
+                </div>
             <?php } ?>
         </div>
 
@@ -83,18 +83,18 @@ $cat = $conn->query("SELECT * FROM `categories` ORDER BY `cname` ASC");
         <div class="row">
             <div class="col-md-12">
                 <div class="owl-carousel owl-theme colleague carousel-indicator">
-                    <?php while($drows=$discount->fetch()){
-                        $pic = explode(',',$drows['images']);?>
+                    <?php while ($drows = $discount->fetch()) {
+                        $pic = explode(',', $drows['images']); ?>
                         <div class="col-md-12 col-12 bg-white p-2 rounded-2 hover-border">
-                            <a href="details.php?id=<?=$drows['pid']?>">
-                            <div class="d-flex justify-content-center">
-                            <img src="img/products/<?=$pic[0]?>" alt="<?=$drows['title']?>" class="img-thumbnail w-75 img-fluid">
-                            </div>
-                            <p class="text-center mt-2 fw-bold text-success"><?=$drows['title']?></p>
-                            <div class="d-flex justify-content-between mt-2">
-                                <span class="text-end text-decoration-line-through text-danger"><?=number_format($drows['price'])?> تومان</span>
-                                <span class="text-start text-dark"><?=number_format($drows['discount'])?> تومان</span>
-                            </div>
+                            <a href="details.php?id=<?= $drows['pid'] ?>">
+                                <div class="d-flex justify-content-center">
+                                    <img src="img/products/<?= $pic[0] ?>" alt="<?= $drows['title'] ?>" class="img-thumbnail w-75 img-fluid">
+                                </div>
+                                <p class="text-center mt-2 fw-bold text-success"><?= $drows['title'] ?></p>
+                                <div class="d-flex justify-content-between mt-2">
+                                    <span class="text-end text-decoration-line-through text-danger"><?= number_format($drows['price']) ?> تومان</span>
+                                    <span class="text-start text-dark"><?= number_format($drows['discount']) ?> تومان</span>
+                                </div>
                             </a>
                         </div>
                     <?php } ?>
@@ -137,7 +137,7 @@ $cat = $conn->query("SELECT * FROM `categories` ORDER BY `cname` ASC");
                         <div class="col-md-4 mt-2 text-center"><img src="img/3.png"></div>
                         <div class="col-md-8">
                             <h6 class="text-dark fw-bold"> زیبا ترین گلدان های تزئینی </h6>
-                            <span>شما در سایت ما  این امکان را دارید که گلدان های تزیینی زیادی را مشاهده و از بین انها زیباترینشان را انتخاب کنید.</span>
+                            <span>شما در سایت ما این امکان را دارید که گلدان های تزیینی زیادی را مشاهده و از بین انها زیباترینشان را انتخاب کنید.</span>
                         </div>
                     </div>
                 </div>
@@ -189,7 +189,7 @@ $cat = $conn->query("SELECT * FROM `categories` ORDER BY `cname` ASC");
         <!-- -->
         <div class="row py-5">
             <div class="col-md-3">
-                <h5 class="font-weight-bold"><i class="fa fa-leaf text-success"></i>  جدیدترین محصولات </h5>
+                <h5 class="font-weight-bold"><i class="fa fa-leaf text-success"></i> جدیدترین محصولات </h5>
             </div>
             <div class="col-md-9">
                 <hr class="text-success">
@@ -197,56 +197,56 @@ $cat = $conn->query("SELECT * FROM `categories` ORDER BY `cname` ASC");
         </div>
         <!-- -->
         <div class="row pb-2">
-           <div class="col-md-12 text-right">
-               <div class="col-md-12">
-                   <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="0">
-                       <div class="carousel-inner">
-                           <?php
-                           $count = 0;
-                           while($rows = $products->fetch()) {
-                               $pic = explode(',',$rows['images']);
-                               $active = '';
-                               if ($count % 4 == 0) {
-                                   $active = ($count == 0) ? 'active' : '';
-                                   if ($count != 0) {
-                                       echo '</div></div>';
-                                   }
-                                   echo '<div class="item carousel-item '.$active.'">';
-                                   echo '<div class="row" style="margin:10px auto;padding: 10px 40px;">';
-                               }
-                               ?>
-                               <div class="col-md-3 mb-2">
-                                   <div class="card p-3">
-                                       <div class="img-box">
-                                           <img src="img/products/<?=$pic['0']?>" class="img-thumbnail img-fluid" alt="<?=$rows['title']?>">
-                                       </div>
-                                       <div>
-                                           <h6 class="small font-weight-bold"><?=$rows['title']?></h6>
-                                           <p class="item-price"><b><?=number_format($rows['price'])?> تومان </b></p>
-                                           <a href="details.php?id=<?=$rows['pid']?>" class="btn btn-sm btn-success">جزئیات</a>
-                                           <a href="details.php?id=<?=$rows['pid']?>&buy" class="btn btn-sm btn-danger">خرید</a>
-                                       </div>
-                                   </div>
-                               </div>
-                               <?php
-                               $count++;
-                           }
-                           if ($count > 0) {
-                               echo '</div></div>';
-                           }
-                           ?>
-                       </div>
-                       <!-- Carousel controls -->
-                       <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
-                           <i class="fa fa-angle-right"></i>
-                       </a>
-                       <a class="carousel-control-next" href="#myCarousel" data-slide="next">
-                           <i class="fa fa-angle-left"></i>
-                       </a>
-                   </div>
-               </div>
-           </div>
-         </div>
+            <div class="col-md-12 text-right">
+                <div class="col-md-12">
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="0">
+                        <div class="carousel-inner">
+                            <?php
+                            $count = 0;
+                            while ($rows = $products->fetch()) {
+                                $pic = explode(',', $rows['images']);
+                                $active = '';
+                                if ($count % 4 == 0) {
+                                    $active = ($count == 0) ? 'active' : '';
+                                    if ($count != 0) {
+                                        echo '</div></div>';
+                                    }
+                                    echo '<div class="item carousel-item ' . $active . '">';
+                                    echo '<div class="row" style="margin:10px auto;padding: 10px 40px;">';
+                                }
+                            ?>
+                                <div class="col-md-3 mb-2">
+                                    <div class="card p-3">
+                                        <div class="img-box">
+                                            <img src="img/products/<?= $pic['0'] ?>" class="img-thumbnail img-fluid" alt="<?= $rows['title'] ?>">
+                                        </div>
+                                        <div>
+                                            <h6 class="small font-weight-bold"><?= $rows['title'] ?></h6>
+                                            <p class="item-price"><b><?= number_format($rows['price']) ?> تومان </b></p>
+                                            <a href="details.php?id=<?= $rows['pid'] ?>" class="btn btn-sm btn-success">جزئیات</a>
+                                            <a href="details.php?id=<?= $rows['pid'] ?>&buy" class="btn btn-sm btn-danger">خرید</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php
+                                $count++;
+                            }
+                            if ($count > 0) {
+                                echo '</div></div>';
+                            }
+                            ?>
+                        </div>
+                        <!-- Carousel controls -->
+                        <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+                            <i class="fa fa-angle-right"></i>
+                        </a>
+                        <a class="carousel-control-next" href="#myCarousel" data-slide="next">
+                            <i class="fa fa-angle-left"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="col-md-6">
                 <div class="custombg rounded p-3 d-flex flex-column flex-md-row justify-content-between align-items-center">
@@ -278,17 +278,17 @@ $cat = $conn->query("SELECT * FROM `categories` ORDER BY `cname` ASC");
         </div>
         <!-- -->
         <div class="row pb-5">
-            <?php while($rows = $articles->fetch()){
-                $pic = explode(',',$rows['pic']);
-                ?>
+            <?php while ($rows = $articles->fetch()) {
+                $pic = explode(',', $rows['pic']);
+            ?>
                 <div class="col-md-6 py-2">
-                    <a href="show.php?id=<?=$rows['id']?>">
+                    <a href="show.php?id=<?= $rows['id'] ?>">
                         <div class="card p-2 btn btn-outline-success">
                             <div class="d-flex">
-                                <img class="img-fluid img-thumbnail w-25 col-md-2"  src="img/article/<?=$rows['pic']?>" alt="<?=$rows['title']?>">
+                                <img class="img-fluid img-thumbnail w-25 col-md-2" src="img/article/<?= $rows['pic'] ?>" alt="<?= $rows['title'] ?>">
                                 <div class="card-body text-start">
-                                    <h6 class="fw-bold"><?=$rows['title']?></h6>
-                                    <p class="text-justify-custom"><?=mb_substr($rows['short'],0,170)?>...</p>
+                                    <h6 class="fw-bold"><?= $rows['title'] ?></h6>
+                                    <p class="text-justify-custom"><?= mb_substr($rows['short'], 0, 170) ?>...</p>
                                 </div>
                             </div>
                         </div>
@@ -301,4 +301,3 @@ $cat = $conn->query("SELECT * FROM `categories` ORDER BY `cname` ASC");
 </main>
 
 <?php include('includes/footer.php'); ?>
-
