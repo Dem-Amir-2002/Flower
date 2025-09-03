2f2b15cff9b8a2dd5c59703be62486a377d49b47
Amir Damirchi <154810287+Dem-Amir-2002@users.noreply.github.com>
2025-09-03 20:43:08 +0330

    adding filters

--- products.php
+++ products.php
@@ -1,22 +1,36 @@
 <?php
 $title = 'فروشگاه گل و گیاه';
 include('includes/header.php');
-$where ='';
-if(isset($_GET['title'])  && !empty($_GET['title']) ){
+
+
+$where = '';
+if (isset($_GET['title'])  && !empty($_GET['title'])) {
     $title = $_GET['title'];
-    $where .= " AND `title` LIKE '%$title%' OR `description` LIKE '%$title%'";
+    $where .= " AND (`title` LIKE '%$title%' OR `description` LIKE '%$title%')";
 }
-if(isset($_GET['cat']) && !empty($_GET['cat'])){
+if (isset($_GET['cat']) && !empty($_GET['cat'])) {
     $catid = $_GET['cat'];
     $where .= " AND `catid`='$catid' ";
 }
-if(isset($_GET['range']) && $_GET['range']!=1180000){
+if (isset($_GET['range']) && $_GET['range'] != 1180000) {
     $price = $_GET['range'];
     $where .= " AND `price`<='$price' ";
 }
+
+$sort = " ORDER BY `created_at` DESC"; // پیش‌فرض
+
+if(isset($_GET['sort']) && !empty($_GET['sort'])){
+    if($_GET['sort'] == "low"){
+        $sort = " ORDER BY `price` ASC";
+    }elseif($_GET['sort'] == "high"){
+        $sort = " ORDER BY `price` DESC";
+    }
+}
+
+
 $cat = $conn->query("SELECT * FROM `categories` ORDER BY `cname` ASC");
 //prodocts list
-$products = $conn->query("SELECT * FROM `products` WHERE TRUE $where AND `status`=1 ORDER BY `created_at` DESC");
+$products = $conn->query("SELECT * FROM `products` WHERE TRUE $where AND `status`=1 $sort");
 $total = $products->rowCount();
 ?>
 <main>
@@ -32,17 +46,27 @@ $total = $products->rowCount();
                     <div class="form-group p-2">
                         <select name="cat" class="form-control">
                             <option value="">دسته بندی محصولات</option>
-                            <?php while ($crows = $cat->fetch()){ ?>
-                                <option value="<?=$crows['cid']?>"><?=$crows['cname']?></option>
+                            <?php while ($crows = $cat->fetch()) { ?>
+                                <option value="<?= $crows['cid'] ?>"><?= $crows['cname'] ?></option>
                             <?php } ?>
                         </select>
                     </div>
                     <div class="form-group p-2">
-                        <div class="px-3 pt-2 bg-white border">
-                            <label for="priceRange" class="form-label">قیمت: <span id="priceValue">50000</span> تومان</label>
+                        <div class="px-3 pt-2 bg ">
+                            <label for="priceRange" class="form-label">حداکثر قیمت: <span id="priceValue">50000</span> تومان</label>
                             <input type="range" name="range" class="form-range" min="350000" max="2000000" step="10000" id="priceRange">
                         </div>
                     </div>
+
+                    <div class="form-group p-2">
+                        <select name="sort" class="form-control">
+                            <option value="">مرتب‌سازی</option>
+                            <option value="low">ارزان‌ترین</option>
+                            <option value="high">گران‌ترین</option>
+                        </select>
+                    </div>
+
+
                     <div class="form-group p-2">
                         <button type="submit" class="btn btn-success w-100">جستجو</button>
                     </div>
@@ -50,33 +74,34 @@ $total = $products->rowCount();
             </div>
             <div class="col-md-9">
                 <div class="row">
-            <?php if($total){ while($p_rows = $products->fetch()){
-                $pic = explode(',',$p_rows['images']);
-                ?>
-                <div class="col-md-4 py-2">
-                <a href="details.php?id=<?=$p_rows['pid']?>">
-                    <div class="card text-right btn btn-outline-secondary border-success">
-                        <div class="d-flex justify-content-center p-2">
-                        <img class="card-img-top img-thumbnail img-fluid" src="img/products/<?=$pic[0]?>" alt="<?=$p_rows['title']?>">
+                    <?php if ($total) {
+                        while ($p_rows = $products->fetch()) {
+                            $pic = explode(',', $p_rows['images']);
+                    ?>
+                            <div class="col-md-4 py-2">
+                                <a href="details.php?id=<?= $p_rows['pid'] ?>">
+                                    <div class="card text-right btn btn-outline-secondary border-success">
+                                        <div class="d-flex justify-content-center p-2">
+                                            <img class="card-img-top img-thumbnail img-fluid" src="img/products/<?= $pic[0] ?>" alt="<?= $p_rows['title'] ?>">
+                                        </div>
+                                        <div class="card-body">
+                                            <p class="fw-bold"><?= $p_rows['title'] ?></p>
+                                            <p><?= number_format($p_rows['price']) ?> تومان</p>
+                                            <?php if ($p_rows['discount'] > 0) { ?>
+                                                <p class="float-left font-weight-bold"><?= number_format($p_rows['discount']) ?> تومان</p>
+                                            <?php } ?>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        <?php }
+                    } else { ?>
+                        <div class="col-md-12 text-center">
+                            <img src="img/empty.png" alt="" width="350">
+                            <p class="font-weight-bold">محتوایی جهت نمایش موجود نمی باشد!</p>
                         </div>
-                        <div class="card-body">
-                            <p class="fw-bold"><?=$p_rows['title']?></p>
-                            <p><?=number_format($p_rows['price'])?> تومان</p>
-                            <?php if($p_rows['discount']>0){ ?>
-                            <p class="float-left font-weight-bold"><?=number_format($p_rows['discount'])?> تومان</p>
-                            <?php } ?>
-                        </div>
-                    </div>
-                </a>
+                    <?php } ?>
                 </div>
-            <?php }
-            }else{ ?>
-                <div class="col-md-12 text-center">
-                    <img src="img/empty.png" alt="" width="350">
-                    <p class="font-weight-bold">محتوایی جهت نمایش موجود نمی باشد!</p>
-                </div>
-        <?php } ?>
-            </div>
             </div>
         </div>
         <!-- -->
